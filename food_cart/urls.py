708fafacb365eb866847963105fec99229574692
Grimmcrow <grimmcrow@localhost.localdomain>
2016-08-24 10:16:33 +0300

    initial commit

---
+++ food_cart/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+
+    url(r'^admin/', admin.site.urls),
+    url(r'^chef/', admin.site.urls),
+    url(r'^cart/', include('cart.urls', namespace='cart')),
+    url(r'^orders/', include('orders.urls', namespace='orders')),
+    url(r'^manager/', include('manager.urls', namespace='manager')),
+    url(r'^', include('food_carts.urls', namespace="food_carts")),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
